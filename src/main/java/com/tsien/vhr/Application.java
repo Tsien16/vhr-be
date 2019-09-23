02c6fb7c727aae1f501a4cb515544d390ea9dd35
tsien16 <tsien16@qq.com>
2019-09-23 14:14:25 +0800

    Initial Commit

--- src/main/java/com/tsien/vhr/Application.java
+++ src/main/java/com/tsien/vhr/Application.java
@@ -3,6 +3,7 @@ package com.tsien.vhr;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 /**
  * Created with IntelliJ IDEA.
@@ -14,6 +15,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 @MapperScan("com.tsien.vhr.dao")
+@EnableSwagger2
 public class Application {
 
     public static void main(String[] args) {
